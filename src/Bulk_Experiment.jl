d6e3cbc025876d9d1894bed73b890f107c34365d
marcoxa <marcoxa@gmail.com>
2022-01-19 22:22:29 +0100

    Cleaned up a bit.  Added doc strings holders.

--- src/Bulk_Experiment.jl
+++ src/Bulk_Experiment.jl
@@ -1,10 +1,20 @@
+### -*- Mode: Julia -*-
+
+### Bulk_Experiment.jl
+
 ########################## Experiments
 #function calculate bulk experiment
-function create_bulk_groundtruth(G_seq::LongDNASeq, Fasta_sample::Vector{Any},
-                                                          Position::Vector{Any})
-    #Dataframe -> POS, REF_nucleotide, %A, %C, %G, %T
-    #=df = DataFrame(Position = Int[], Reference = DNA[],  A = Any[], C = Any[],
-                                     G = Any[], T = Any[])
+
+"Function create_bulk_groundtruth
+
+"
+function create_bulk_groundtruth(G_seq::LongDNASeq,
+                                 Fasta_sample::Vector{Any},
+                                 Position::Vector{Any})
+    # Dataframe -> POS, REF_nucleotide, %A, %C, %G, %T
+    #=
+    df = DataFrame(Position = Int[], Reference = DNA[],  A = Any[], C = Any[],
+                   G = Any[], T = Any[])
     positions = []
     n_sample = length(Fasta_sample)
     for i = 1:len_ROI
@@ -25,7 +35,9 @@ function create_bulk_groundtruth(G_seq::LongDNASeq, Fasta_sample::Vector{Any},
             end
             push!(df, [pos, nucl_ref, values[1], values[2], values[3], values[4]])
         end
-    end=#
+    end
+    =#
+    
     df = DataFrame(MUT = Any[], VAF = AbstractFloat[])
     n_sample = length(Fasta_sample)
     Position_sort = sort(Position)
@@ -35,12 +47,12 @@ function create_bulk_groundtruth(G_seq::LongDNASeq, Fasta_sample::Vector{Any},
         nucl_counter = counter(nucleotides)
         k_dict = keys(nucl_counter.map)
         for k in k_dict
-            #println("k :",k,"\t",typeof(k))
-            #println("k != nucl_ref -> ",k != nucl_ref)
+            # println("k :",k,"\t",typeof(k))
+            # println("k != nucl_ref -> ",k != nucl_ref)
             if k != nucl_ref
                 mut = string(pos) * "_" * string(nucl_ref) * "->" * string(k)
                 vaf = nucl_counter[k]/n_sample
-                #println("vaf -> ",vaf)
+                # println("vaf -> ",vaf)
                 push!(df, [mut, vaf])
             end
         end
@@ -48,39 +60,56 @@ function create_bulk_groundtruth(G_seq::LongDNASeq, Fasta_sample::Vector{Any},
     return df
 end
 
+
+"Function bulk_with_noise
+
+"
 function bulk_with_noise(Coverage, VAF, n_sample, FP, FN, Positions)
     df_noise = DataFrame(MUT = Any[], VAF = AbstractFloat[])
     for i in 1:length(Positions)
         n_reads = rand(Poisson(Coverage), 1)[1]
-        #println("n_reads: ", n_reads)
+        # println("n_reads: ", n_reads)
         n_read_muts = rand(Binomial(n_reads, VAF[i, :VAF]), 1)[1]
-        #println("n_read_muts: ", n_read_muts)
+        # println("n_read_muts: ", n_read_muts)
         n_FP = rand(Binomial(n_sample-n_read_muts, FP), 1)[1]
-        #println("n_FP: ",n_FP)
+        # println("n_FP: ",n_FP)
         n_FN = rand(Binomial(n_read_muts, FN), 1)[1]
-        #println("n_FN: ",n_FN)
+        # println("n_FN: ",n_FN)
         n_VAF_Noise = (n_read_muts + n_FP - n_FN)/n_sample
-        #println("n_VAF_Noise: ", n_VAF_Noise)
+        # println("n_VAF_Noise: ", n_VAF_Noise)
         push!(df_noise, [VAF[i, :MUT], n_VAF_Noise])
     end
     return df_noise
 end
 
-function experiment_bulk(reference::LongDNASeq, fasta_samples::Vector{Any},
-                            position_used::Vector{Any}; Noise::Bool=false,
-                                coverage::AbstractFloat=0, FP::AbstractFloat=0,
-                                                            FN::AbstractFloat=0)
-    #calculate bulk experiment if bulk is true
+
+"Function experiment_bulk
+
+"
+function experiment_bulk(reference::LongDNASeq,
+                         fasta_samples::Vector{Any},
+                         position_used::Vector{Any};
+                         Noise::Bool = false,
+                         coverage::AbstractFloat = 0,
+                         FP::AbstractFloat = 0,
+                         FN::AbstractFloat = 0)
+    # calculate bulk experiment if bulk is true
     VAF_GT = create_bulk_groundtruth(reference, fasta_samples, position_used)
-    CSV.write("VAF_GT.vcf", VAF_GT, delim=",")
+    CSV.write("VAF_GT.vcf", VAF_GT, delim = ",")
     if Noise
         if coverage == 0 && FP == 0 && FN == 0
             return "Error: insert covarege, FP and FN"
         end
         n_sample = length(fasta_samples)
-        VAF_GT_Noise = bulk_with_noise(coverage, VAF_GT, n_sample, FP, FN,
-                                                                  position_used)
-        CSV.write("VAF_GT_Noise.vcf", VAF_GT_Noise, delim=",")
+        VAF_GT_Noise = bulk_with_noise(coverage,
+                                       VAF_GT,
+                                       n_sample,
+                                       FP,
+                                       FN, position_used)
+        CSV.write("VAF_GT_Noise.vcf", VAF_GT_Noise, delim = ",")
     end
-    return VAF_GT, VAF_GT_Noise#, g_seq
+    return VAF_GT, VAF_GT_Noise #, g_seq
 end
+
+
+### end of file -- Bulk_Experiment.jl
