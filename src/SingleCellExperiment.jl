8e686a1f0ac3bf2154997f271af375ccf05de4da
marcoxa <marcoxa@gmail.com>
2022-01-21 00:03:07 +0100

    Clened up and and added doc string; contains an error.

--- src/SingleCellExperiment.jl
+++ src/SingleCellExperiment.jl
@@ -1,55 +1,77 @@
-#function that compute num of mutation for all edges
-function evolution_seq(Tree::AbstractMetaGraph, Neutral_mut_rate::AbstractFloat,
-                                                                length_ROI::Int)
+### -*- Mode: Julia -*-
+
+### SingleCellExperiment.jl
+
+"""
+Computes number of mutations for all edges.
+"""
+function evolution_seq(Tree::AbstractMetaGraph,
+                       Neutral_mut_rate::AbstractFloat, # AbstractFloat?
+                       length_ROI::Int)
     mutations = []
-    #println("evolution_seq funzione")
+    ## println("evolution_seq funzione")
+
     for e in edges(Tree)
-        Tfinal = get_prop(Tree, dst(e), :Time)#prima era len = ...
-    #   println("len: ",len)
-        λ = Neutral_mut_rate * length_ROI   #cambiato
-    #   println("valore λ: ", λ)
-    #   n_mut = rand(Poisson(λ), 1)[1]
-    #   println("num mut dalla distribuzione di poisson: ",n_mut)
-    ### modifica #####
+        Tfinal = get_prop(Tree, dst(e), :Time) # Prima era len = ...
+        ## println("len: ",len)
+
+        λ = Neutral_mut_rate * length_ROI # Cambiato
+
+        ## println("valore λ: ", λ)
+        ## n_mut = rand(Poisson(λ), 1)[1]
+        ## println("num mut dalla distribuzione di poisson: ", n_mut)
+
+        ## Modifica!!!
         t_curr = 0
         n_mut = 0
         while t_curr < Tfinal
-            t_curr = t_curr + rand(Exponential(1/λ), 1)[1]
+            t_curr = t_curr + rand(Exponential(1 / λ), 1)[1]
             n_mut += 1
         end
-        push!(mutations,n_mut)
+        push!(mutations, n_mut)
         set_prop!(Tree, src(e), dst(e), :N_Mutations, n_mut)
     end
     return mutations
 end
 
-#change genome in position on pos
+
+"""
+Change genome in position pos.
+"""
 function transform_genome(genome::LongDNASeq, pos::Vector{Any})
-    transition_matrix = DataFrame(A = [0.0, 0.33333333, 0.33333333, 0.33333333],
-                                  C = [0.33333333, 0.0, 0.33333333, 0.33333333],
-                                  G = [0.33333333, 0.33333333, 0.0, 0.33333333],
-                                  T = [0.33333333, 0.33333333, 0.33333333, 0.0])
+    transition_matrix =
+        DataFrame(A = [0.0, 0.33333333, 0.33333333, 0.33333333],
+                  C = [0.33333333, 0.0, 0.33333333, 0.33333333],
+                  G = [0.33333333, 0.33333333, 0.0, 0.33333333],
+                  T = [0.33333333, 0.33333333, 0.33333333, 0.0])
     for p in pos
-        #println("pos: ",p)
+        ## println("pos: ",p)
         nucleotide = genome[p]
-        #println("subsequence: ",genome[p-2:p+2])
-        #println("nuclotide: ",nucleotide)
-        #come nella simulatione, ho una certa prob che avvenga una simulatione
+        ## println("subsequence: ",genome[p - 2 : p + 2])
+        ## println("nuclotide: ",nucleotide)
+
+        ## Come nella simulatione, ho una certa prob che avvenga una
+        ## simulatione
         prob_cum = cumsum(transition_matrix[!, string(nucleotide)])
-        #println("prob_cum: ",prob_cum)
-        k = round( rand(), digits=7) # with 7 decimals
+        
+        ## println("prob_cum: ",prob_cum)
+        k = round( rand(), digits = 7) # with 7 decimals
         target_nucl = collect(k .<= prob_cum)
-        #println("target_nucl: ",target_nucl)
+        ## println("target_nucl: ",target_nucl)
         min = findfirst(target_nucl)
-        #println("min: ",min)
-        new_nucleotide = collect(names(transition_matrix)[min])[1]#type char
-        #println("new_nucleotide: ",new_nucleotide)
+        ## println("min: ",min)
+        new_nucleotide = collect(names(transition_matrix)[min])[1] # type char
+        ## println("new_nucleotide: ",new_nucleotide)
         genome[p] = DNA(new_nucleotide)
     end
     return genome
 end
 
-#check that num of mutation for each path (root -> leaf) is less than len_ROI
+
+"""
+Checks that num of mutation for each path (root -> leaf) is less than
+len_ROI
+"""
 function check_num_path_mutation(Tree::AbstractMetaGraph, len_ROI::Int)
     leafs = get_leafs(Tree)
     check = false
@@ -59,7 +81,7 @@ function check_num_path_mutation(Tree::AbstractMetaGraph, len_ROI::Int)
         path = yen_k.paths[1]
         sum = 0
         for v in 1:length(path)-1
-            sum = sum + get_prop(Tree, path[v], path[v+1], :N_Mutations)
+            sum = sum + get_prop(Tree, path[v], path[v + 1], :N_Mutations)
         end
         if sum > len_ROI
             check = true
@@ -69,12 +91,16 @@ function check_num_path_mutation(Tree::AbstractMetaGraph, len_ROI::Int)
 end
 
 
-#create a input for tool ART -> FASTA file and tree on format newick
-function Molecular_evolution(Tree::AbstractMetaGraph, neural_mut_rate::Float64;
-                                         path::String = "", len_ROI::Int = 6000,
-                                         single_cell::Bool=true)
+"""
+Creates a input for tool ART -> FASTA file and tree on format Newick.
+"""
+function Molecular_evolution(Tree::AbstractMetaGraph,
+                             neural_mut_rate::Float64;
+                             path::String = "",
+                             len_ROI::Int = 6000, # Why 6000?
+                             single_cell::Bool = true)
 
-    #crete/load reference genome
+    ## Create/load reference genome
     if path != ""
         open(FASTA.Reader, path) do reader
             for record in reader
@@ -90,57 +116,75 @@ function Molecular_evolution(Tree::AbstractMetaGraph, neural_mut_rate::Float64;
         close(w)
     end
 
-    #compute mutations ∀ node
+    ## Compute mutations ∀ node
     for i in 1:3
         n_mutations = evolution_seq(Tree, neural_mut_rate, len_ROI)
         check = check_num_path_mutation(Tree, len_ROI)
         if check == true && i >= 3
-            return "ERROR: mutations are more than lenght ROI -> (file fasta)"
+            ## If it is an error, signal one!
+            return "ERROR: mutations are more than lenght ROI -> (file fasta)."
         else
             break
-    end
-    possible_position = Set(1:len_ROI)
-    #g_seq_e = copy(g_seq) #reference g_seq not change
-    position_used = []
-    #create fasta ∀ nodes
-    for e in edges(Tree)
-        g_seq_e = LongDNASeq()
-        if has_prop(Tree, src(e), :Fasta)
-            #println("il source ha un file Fasta")
-            g_seq_e = copy(get_prop(Tree, src(e), :Fasta))
-        else
-            g_seq_e = copy(g_seq)
         end
-        n_mut = get_prop(Tree, e, :N_Mutations)
-        pos_edge = []
-        for i=1:n_mut
-            pos = rand(possible_position)
-            delete!(possible_position, pos)
-            push!(pos_edge, pos)
-            push!(position_used, pos)
+
+        ## IMHO manca una 'end' qui!!!!
+
+        possible_position = Set(1:len_ROI)
+        # g_seq_e = copy(g_seq) #reference g_seq not change
+
+        position_used = []
+
+        # create fasta ∀ nodes
+        for e in edges(Tree)
+            g_seq_e = LongDNASeq()
+            if has_prop(Tree, src(e), :Fasta)
+                # println("il source ha un file Fasta")
+                g_seq_e = copy(get_prop(Tree, src(e), :Fasta))
+            else
+                g_seq_e = copy(g_seq)
+            end
+            n_mut = get_prop(Tree, e, :N_Mutations)
+            pos_edge = []
+            for i = 1:n_mut
+                pos = rand(possible_position)
+                delete!(possible_position, pos)
+                push!(pos_edge, pos)
+                push!(position_used, pos)
+            end
+            g_seq_e = transform_genome(g_seq_e, pos_edge)
+            set_prop!(Tree, dst(e), :Fasta, g_seq_e)
         end
-        g_seq_e = transform_genome(g_seq_e, pos_edge)
-        set_prop!(Tree, dst(e), :Fasta, g_seq_e)
+
+        # Return fasta of leaf nodes
+        leafs = get_leafs(Tree)
+        fasta_samples = []
+        for l in leafs
+            f = get_prop(Tree, l, :Fasta)
+            push!(fasta_samples, f)
+        end
+
+        ## write fasta on files if single_cell is true
+        if single_cell
+            mkpath("Fasta output") # Create folder
+            for i in 1:length(leafs)
+                
+                ## Windows-ism!!!!
+                
+                w = FASTA.Writer(open("Fasta output\\sample"
+                                      * string(leafs[i])
+                                      * ".fasta",
+                                      "w"))
+                rec = FASTA.Record("Sample"
+                                   * string(leafs[i]),
+                                   fasta_samples[i])
+                write(w, rec)
+                close(w)
+            end
+        end
+        return g_seq, fasta_samples, position_used
     end
 
-    #return fasta of leaf nodes
-    leafs = get_leafs(Tree)
-    fasta_samples = []
-    for l in leafs
-        f = get_prop(Tree, l, :Fasta)
-        push!(fasta_samples, f)
-    end
+    ### E lo strumento superiore mi fa immediatamente vedere che
+    ### qui... Houston we have a problem.
 
-    #write fasta on files if single_cell is true
-    if single_cell
-        mkpath("Fasta output")#create folder
-        for i in 1:length(leafs)
-            w = FASTA.Writer(open("Fasta output\\sample" * string(leafs[i])
-                                                               * ".fasta", "w"))
-            rec = FASTA.Record("Sample" * string(leafs[i]), fasta_samples[i])
-            write(w, rec)
-            close(w)
-        end
-    end
-    return g_seq, fasta_samples, position_used
-end
+    ### end of file -- SingleCellExperiment.jl
